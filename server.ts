e951437603a330d343f9f49bc1fe1c5b75aeaa69
gilpop8663 <wolfye@naver.com>
2022-03-01 13:21:45 +0900

    Add - servet.ts post 추가

--- server.ts
+++ server.ts
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 const port = process.env.PORT || 4000;
+const { users, messages } = require('./db.json');
 
 server.use(middlewares);
 server.use(
@@ -12,6 +13,20 @@ server.use(
     '/api/*': '/$1',
   })
 );
+
+server.post('/login', (req, res) => {
+  console.log(req.body);
+  const { id, password } = req.body;
+  const userInfo = users.find((e) => {
+    return e.id === id && e.password === password;
+  });
+  console.log(userInfo);
+  if (userInfo) {
+    res.send(userInfo);
+  } else {
+    res.send();
+  }
+});
 
 server.use(router);
 
